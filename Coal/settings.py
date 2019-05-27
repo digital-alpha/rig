a55b8216c29fd9731f2b59057ffd7decaa5928c1
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-05-27 19:23:38 +0530

    debugging
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- Coal/settings.py
+++ Coal/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://co-al.herokuapp.com']
 
