4f649f654763708521605686f63a8a9e93b2c11c
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-05-27 18:08:20 +0530

    setup done
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- Coal/settings.py
+++ Coal/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import django_heroku
 
+from decouple import config
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -24,10 +25,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['co-al.herokuapp.com', '127.0.0.1']
 
