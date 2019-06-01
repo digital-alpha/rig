5374087c27a4b837cd1e01a27883587018344a3f
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-06-01 19:48:53 +0530

    configure static
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- Coal/settings.py
+++ Coal/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticRoot')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
--- UploadMulti/static/UploadMulti/js/basic-upload.js
+++ UploadMulti/static/UploadMulti/js/basic-upload.js
@@ -3,14 +3,14 @@ $(function () {
         $("#fileupload").click();
     });
 
-    $("#fileupload").fileupload({
-        dataType: 'json',
-        done: function (e, data) {
-              if (data.result.is_valid) {
-                  $("#gallery tbody").prepend(
-                      "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
-                  )
-            }
-        }
-    });
+    // $("#fileupload").fileupload({
+    //     dataType: 'json',
+    //     done: function (e, data) {
+    //           if (data.result.is_valid) {
+    //               $("#gallery tbody").prepend(
+    //                   "<tr><td><a href='" + data.result.url + "'>" + data.result.name + "</a></td></tr>"
+    //               )
+    //         }
+    //     }
+    // });
 });
