e732ff62f5313dee49a21575d23c699ce09c09ff
Siddharth Pahuja <sid.pahuja2008@gmail.com>
2019-08-17 00:58:27 +0530

    upload api enhanced
    Signed-off-by: Siddharth Pahuja <sid.pahuja2008@gmail.com>

--- UploadMulti/API/serializers.py
+++ UploadMulti/API/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from UploadMulti.models import Document, Detail
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
 class DocumentSerializer(serializers.ModelSerializer):
+        # Create a custom method field
+    uploaded_by = serializers.SerializerMethodField('_user')
+
+    # Use this method for the custom field
+    def _user(self, obj):
+        return self.context['request'].user.username
     class Meta:
         model = Document
-        fields = ('file','id')
+        fields = ('file','id', 'uploaded_at', 'uploaded_by')
+
 
 class DetailSerializer(serializers.ModelSerializer):
     class Meta:
